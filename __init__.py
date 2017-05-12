3aff70ed0ad868aef9c41c5bb4e80c38eb49347e
Nikita Akimov <Korchy@users.noreply.github.com>
2017-05-12 22:28:17 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,45 @@
+bl_info = {
+    'name': 'BIS',
+    'category': 'Material',
+    'author': 'Nikita Akimov',
+    'version': (0, 0, 1),
+    'blender': (2, 78, 0),
+    'location': 'T-Panel > BIS',
+    'wiki_url': 'http://b3d.interplanety.ru/bis/',
+    'tracker_url': 'http://b3d.interplanety.ru/bis/',
+    'description': 'BIS - Blender Interplanety Store'
+}
+
+import sys
+import importlib
+
+modulesNames = ['BIS_addNodeToStore', 'BIS_getNodeFromStore', 'BIS_getNodesFromStore', 'WebRequests', 'BIS_mainPanel', 'MessageBox', 'NodeManager']
+
+modulesFullNames = {}
+for currentModuleName in modulesNames:
+    if 'DEBUG_MODE' in sys.argv:
+        modulesFullNames[currentModuleName] = ('{}'.format(currentModuleName))
+    else:
+        modulesFullNames[currentModuleName] = ('{}.{}'.format(__name__, currentModuleName))
+
+for currentModuleFullName in modulesFullNames.values():
+    if currentModuleFullName in sys.modules:
+        importlib.reload(sys.modules[currentModuleFullName])
+    else:
+        globals()[currentModuleFullName] = importlib.import_module(currentModuleFullName)
+        setattr(globals()[currentModuleFullName], 'modulesNames', modulesFullNames)
+
+def register():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'register'):
+                sys.modules[currentModuleName].register()
+
+def unregister():
+    for currentModuleName in modulesFullNames.values():
+        if currentModuleName in sys.modules:
+            if hasattr(sys.modules[currentModuleName], 'unregister'):
+                sys.modules[currentModuleName].unregister()
+
+if __name__ == "__main__":
+    register()
