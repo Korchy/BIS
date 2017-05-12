d035ac18782edeb7cccc0713ffb6102ec0dc9679
Nikita Akimov <Korchy@users.noreply.github.com>
2017-05-12 22:24:59 +0300

    Create BIS_getNodeFromStore.py

---
+++ BIS_getNodeFromStore.py
@@ -0,0 +1,35 @@
+import bpy
+import sys
+import json
+
+class BIS_getNodeFromStore(bpy.types.Operator):
+    bl_idname = 'bis.get_node_from_store'
+    bl_label = 'BIS_GetFromStore'
+    bl_description = 'Get nodegroup from common part of BIS'
+    bl_options = {'REGISTER', 'UNDO'}
+
+    nodeGroupId = bpy.props.IntProperty(
+        name = 'NodeGroupId',
+        default = 0
+    )
+
+    def execute(self, context):
+        if(self.nodeGroupId):
+            request = sys.modules[modulesNames['WebRequests']].WebRequest.sendRequest({
+                'for': 'get_node_group',
+                'id': self.nodeGroupId
+            })
+            requestRez = json.loads(request.text)
+            if requestRez['stat'] != 'T':
+                bpy.ops.message.messagebox('INVOKE_DEFAULT', message = requestRez['stat'])
+            else:
+                sys.modules[modulesNames['NodeManager']].NodeManager.jsonToNodeGroup(requestRez['data'])
+        else:
+            bpy.ops.message.messagebox('INVOKE_DEFAULT', message = 'No NodeGroup To Get')
+        return {'FINISHED'}
+
+def register():
+    bpy.utils.register_class(BIS_getNodeFromStore)
+
+def unregister():
+    bpy.utils.unregister_class(BIS_getNodeFromStore)
