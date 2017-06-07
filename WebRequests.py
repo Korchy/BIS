c5b1d8326d1817bae8f00484596bd32e4b6344d3
Nikita Akimov <Korchy@users.noreply.github.com>
2017-06-07 17:05:46 +0300

    Update WebRequests.py

--- WebRequests.py
+++ WebRequests.py
@@ -5,28 +5,21 @@ import requests
 
 class WebAuthVars(bpy.types.PropertyGroup):
     logged = bpy.props.BoolProperty(
-        name = 'Logged',
-        description = 'Is user logged',
         default = False
     )
     host = bpy.props.StringProperty(
-        name = 'Host',
-        description = 'Host',
         default = ''
     )
-    userLoginSaved = bpy.props.StringProperty(
-        name = 'User Login Saved',
-        description = 'Saved user login',
+    userLogin = bpy.props.StringProperty(
         default = ''
+    )
+    userStayLogged = bpy.props.BoolProperty(
+        default = False
     )
     token = bpy.props.StringProperty(
-        name = 'Token',
-        description = 'Token',
         default = ''
     )
     requestBase = bpy.props.StringProperty(
-        name = 'Request Base',
-        description = 'Request base',
         default = ''
     )
 
@@ -45,50 +38,83 @@ class WebAuth(bpy.types.Operator):
         description = "User Password",
         default = ''
     )
-    WebAuthVars.logged = False
-    WebAuthVars.host = ''
-    WebAuthVars.userLoginSaved = ''
-    WebAuthVars.token = ''
-    WebAuthVars.requestBase = ''
+    userStayLogged = bpy.props.BoolProperty(
+        name = 'Stay logged (insecure)',
+        description = 'Stay logged',
+        default = False
+    )
 
     def execute(self, context):
-        self.logIn()
+        if WebAuthVars.logged:
+            self.logOff()
+        else:
+            self.logIn()
         for area in bpy.context.screen.areas:
             area.tag_redraw()
         return {'FINISHED'}
 
     def invoke(self, context, event):
-        self.logOff()
-        if WebAuthVars.userLoginSaved != '':
-            self.userLogin = WebAuthVars.userLoginSaved
-        return context.window_manager.invoke_props_dialog(self)
+        if WebAuthVars.logged:
+            return self.execute(context)
+        else:
+            if WebAuthVars.userLogin:
+                self.userLogin = WebAuthVars.userLogin
+            return context.window_manager.invoke_props_dialog(self)
 
     @classmethod
     def getInitData(cls):
+        WebAuthVars.logged = False
+        WebAuthVars.userStayLogged = False
         with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + 'config.json') as currentFile:
             jsonData = json.load(currentFile)
             WebAuthVars.host = jsonData['host']
             WebAuthVars.requestBase = jsonData['requestbase']
             if 'userLogin' in jsonData:
-                WebAuthVars.userLoginSaved = jsonData['userLogin']
+                WebAuthVars.userLogin = jsonData['userLogin']
+            else:
+                WebAuthVars.userLogin = ''
+            if 'token' in jsonData:
+                WebAuthVars.token = jsonData['token']
+            else:
+                WebAuthVars.token = ''
+            currentFile.close()
+        if WebAuthVars.token:
+            cls.logIn(cls)
 
     def logIn(self):
-        session = WebRequestsVars.getSession()
-        data = {'requestbase': WebAuthVars.requestBase, 'userlogin': self.userLogin, 'userpassword': self.userPassword}
-        self.userPassword = ''
-        request = session.post(WebAuthVars.host+'/blender_auth', data = data)
-        requestRez = json.loads(request.text)
-        if requestRez['stat'] == 'T':
+        if WebAuthVars.token:
             WebAuthVars.logged = True
-            WebAuthVars.token = requestRez['token']
         else:
-            bpy.ops.message.messagebox('INVOKE_DEFAULT', message = requestRez['txt'])
-            self.logOff()
+            session = WebRequestsVars.getSession()
+            data = {'requestbase': WebAuthVars.requestBase, 'userlogin': self.userLogin, 'userpassword': self.userPassword}
+            self.userPassword = ''
+            request = session.post(WebAuthVars.host+'/blender_auth', data = data)
+            requestRez = json.loads(request.text)
+            if requestRez['stat'] == 'T':
+                WebAuthVars.logged = True
+                WebAuthVars.token = requestRez['token']
+                WebAuthVars.userLogin = self.userLogin
+                self.saveConfig(userLogin = WebAuthVars.userLogin,
+                                token = WebAuthVars.token if self.userStayLogged else '')
+            else:
+                bpy.ops.message.messagebox('INVOKE_DEFAULT', message = requestRez['txt'])
+                self.logOff()
 
     def logOff(self):
         WebAuthVars.token = ''
         WebAuthVars.logged = False
         WebRequestsVars.closeSession()
+        self.saveConfig(userLogin = WebAuthVars.userLogin)
+
+    def saveConfig(self, userLogin = '', token = ''):
+        with open(os.path.dirname(os.path.abspath(__file__)) + os.sep + 'config.json', 'r+') as configFile:
+            jsonData = json.load(configFile)
+            jsonData['token'] = token
+            jsonData['userLogin'] = userLogin
+            configFile.seek(0)
+            configFile.truncate()
+            json.dump(jsonData, configFile, indent = 4)
+            configFile.close()
 
 class WebRequestsVars():
     session = None
@@ -110,7 +136,8 @@ class WebRequest():
     def sendRequest(data = {}):
         requestData = {'requestbase': WebAuthVars.requestBase, 'token': WebAuthVars.token}
         requestData.update(data)
-        request = WebRequestsVars.session.post(WebAuthVars.host+'/blender_request', data = requestData)
+        session = WebRequestsVars.getSession()
+        request = session.post(WebAuthVars.host+'/blender_request', data = requestData)
         return request
 
 def register():
