369f75af60bf9b281e7d7df0483e78c368910ce3
Nikita Akimov <Korchy@users.noreply.github.com>
2017-07-01 11:41:36 +0300

    Update BIS_getNodeFromStorage.py

--- BIS_getNodeFromStorage.py
+++ BIS_getNodeFromStorage.py
@@ -19,29 +19,30 @@ class BIS_getNodeFromStorage(bpy.types.Operator):
                 'for': 'get_node_group',
                 'id': self.nodeGroupId
             })
-            requestRez = json.loads(request.text)
-            if requestRez['stat'] != 'OK':
-                bpy.ops.message.messagebox('INVOKE_DEFAULT', message = requestRez['stat']['text'])
-            else:
-                nodeInJson = json.loads(requestRez['data']['item'])
-                destNodeTree = None
-                if nodeInJson['bl_type'] == 'CompositorNodeGroup':
-                    if bpy.context.area.spaces.active.node_tree.bl_idname == 'CompositorNodeTree':
-                        destNodeTree = bpy.context.area.spaces.active.node_tree
-                        if not bpy.context.screen.scene.use_nodes:
-                            bpy.context.screen.scene.use_nodes = True
-                elif nodeInJson['bl_type'] == 'ShaderNodeGroup':
-                    if bpy.context.active_object:
-                        if not bpy.context.active_object.active_material:
-                            bpy.context.active_object.active_material = bpy.data.materials.new(name = 'Material')
-                            bpy.context.active_object.active_material.use_nodes = True
-                            for currentNode in bpy.context.active_object.active_material.node_tree.nodes:
-                                if currentNode.bl_idname != 'ShaderNodeOutputMaterial':
-                                    bpy.context.active_object.active_material.node_tree.nodes.remove(currentNode)
-                        if bpy.context.area.spaces.active.node_tree.bl_idname == 'ShaderNodeTree':
-                            destNodeTree = bpy.context.active_object.active_material.node_tree
-                if nodeInJson and destNodeTree:
-                    sys.modules[modulesNames['NodeManager']].NodeManager.jsonToNodeGroup(destNodeTree, nodeInJson)
+            if request:
+                requestRez = json.loads(request.text)
+                if requestRez['stat'] != 'OK':
+                    bpy.ops.message.messagebox('INVOKE_DEFAULT', message = requestRez['stat']['text'])
+                else:
+                    nodeInJson = json.loads(requestRez['data']['item'])
+                    destNodeTree = None
+                    if nodeInJson['bl_type'] == 'CompositorNodeGroup':
+                        if bpy.context.area.spaces.active.node_tree.bl_idname == 'CompositorNodeTree':
+                            destNodeTree = bpy.context.area.spaces.active.node_tree
+                            if not bpy.context.screen.scene.use_nodes:
+                                bpy.context.screen.scene.use_nodes = True
+                    elif nodeInJson['bl_type'] == 'ShaderNodeGroup':
+                        if bpy.context.active_object:
+                            if not bpy.context.active_object.active_material:
+                                bpy.context.active_object.active_material = bpy.data.materials.new(name = 'Material')
+                                bpy.context.active_object.active_material.use_nodes = True
+                                for currentNode in bpy.context.active_object.active_material.node_tree.nodes:
+                                    if currentNode.bl_idname != 'ShaderNodeOutputMaterial':
+                                        bpy.context.active_object.active_material.node_tree.nodes.remove(currentNode)
+                            if bpy.context.area.spaces.active.node_tree.bl_idname == 'ShaderNodeTree':
+                                destNodeTree = bpy.context.active_object.active_material.node_tree
+                    if nodeInJson and destNodeTree:
+                        sys.modules[modulesNames['NodeManager']].NodeManager.jsonToNodeGroup(destNodeTree, nodeInJson)
         else:
             bpy.ops.message.messagebox('INVOKE_DEFAULT', message = 'No NodeGroup To Get')
         return {'FINISHED'}
