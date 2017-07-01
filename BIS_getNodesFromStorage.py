e740411d50b1c9e009236d170627653eda183fce
Nikita Akimov <Korchy@users.noreply.github.com>
2017-07-01 11:40:42 +0300

    Update BIS_getNodesFromStorage.py

--- BIS_getNodesFromStorage.py
+++ BIS_getNodesFromStorage.py
@@ -14,19 +14,21 @@ class BIS_getNodesInfoFromStorage(bpy.types.Operator):
             'search_filter': bpy.context.window_manager.bis_get_nodes_info_from_storage_vars.searchFilter,
             'update_preview': bpy.context.window_manager.bis_get_nodes_info_from_storage_vars.updatePreviews
         })
-        searchRez = json.loads(request.text)
-        if searchRez['stat'] == 'OK':
-            previewToUpdate = sys.modules[modulesNames['BIS_Items']].BIS_Items.updatePreviewsFromData(searchRez['data']['items'], context.area.spaces.active.type)
-            if previewToUpdate:
-                request = sys.modules[modulesNames['WebRequests']].WebRequest.sendRequest({
-                    'for': 'update_previews',
-                    'preview_list': previewToUpdate,
-                    'storage_type': context.area.spaces.active.type
-                })
-                previewsUpdateRez = json.loads(request.text)
-                if previewsUpdateRez['stat'] == 'OK':
-                    sys.modules[modulesNames['BIS_Items']].BIS_Items.updatePreviewsFromData(previewsUpdateRez['data']['items'], context.area.spaces.active.type)
-            sys.modules[modulesNames['BIS_Items']].BIS_Items.createItemsList(searchRez['data']['items'], context.area.spaces.active.type)
+        if request:
+            searchRez = json.loads(request.text)
+            if searchRez['stat'] == 'OK':
+                previewToUpdate = sys.modules[modulesNames['BIS_Items']].BIS_Items.updatePreviewsFromData(searchRez['data']['items'], context.area.spaces.active.type)
+                if previewToUpdate:
+                    request = sys.modules[modulesNames['WebRequests']].WebRequest.sendRequest({
+                        'for': 'update_previews',
+                        'preview_list': previewToUpdate,
+                        'storage_type': context.area.spaces.active.type
+                    })
+                    if request:
+                        previewsUpdateRez = json.loads(request.text)
+                        if previewsUpdateRez['stat'] == 'OK':
+                            sys.modules[modulesNames['BIS_Items']].BIS_Items.updatePreviewsFromData(previewsUpdateRez['data']['items'], context.area.spaces.active.type)
+                sys.modules[modulesNames['BIS_Items']].BIS_Items.createItemsList(searchRez['data']['items'], context.area.spaces.active.type)
         return {'FINISHED'}
 
 class BIS_getNodesInfoFromStorageVars(bpy.types.PropertyGroup):
