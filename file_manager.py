ecd3b389d22e148838b82a50b5508627d9663514
Nikita Akimov <korchiy@yandex.ru>
2019-05-24 17:08:55 +0300

    update

--- file_manager.py
+++ file_manager.py
@@ -3,10 +3,12 @@
 
 # File manager
 import os
+import re
 import bpy
 
 
 class FileManager:
+
     @staticmethod
     def abs_path(path):
         # returns absolute file path from path
@@ -14,3 +16,14 @@ class FileManager:
             return os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(bpy.data.filepath)), path[2:]))
         else:
             return os.path.abspath(path)
+
+    @staticmethod
+    def normalize_file_name(source_file_name):
+        # returns corrected file name from name with index (xx.jpg.001 -> xx.001.jpg)
+        regexp = re.compile(r'(\.\d{3}$)')
+        splitted_name = list(filter(None, regexp.split(source_file_name)))
+        if len(splitted_name) > 1:
+            name_ext = os.path.splitext(splitted_name[0])
+            return name_ext[0] + splitted_name[1] + name_ext[1]
+        else:
+            return source_file_name
